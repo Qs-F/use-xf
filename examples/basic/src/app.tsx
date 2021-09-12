602daec8c0bdb849ba86f3e02ede077c86f20a10
Qs-F <qsf@de-liker.com>
2021-09-12 19:32:20 +0900

    examples/basic: Update example

--- examples/basic/src/app.tsx
+++ examples/basic/src/app.tsx
@@ -1,11 +1,64 @@
-import { FC, Fragment } from 'react'
+import { VFC, Fragment } from 'react'
 import { useXf } from '@creatorqsf/use-xf'
 
-export const App: FC = () => {
-  const [state, setState] = useXf((n: number): string => n.toString(), 0)
+const CounterExample: VFC = () => {
+  const [state, setState] = useXf(
+    (n: number): string => 'count: ' + n.toString(),
+    0
+  )
+
   return (
     <Fragment>
-      <p onClick={() => setState(10)}>{state}</p>
+      <button onClick={() => setState(10)}>Make counter 10</button>
+      <p>{state}</p>
     </Fragment>
   )
 }
+
+const EnglishCounterExample: VFC = () => {
+  const [state, setState] = useXf((str?: string):
+    | 'not number'
+    | '1st'
+    | '2nd'
+    | '3rd'
+    | `${number}th` => {
+    if (!str) {
+      return 'not number'
+    }
+    const n = parseInt(str)
+    if (isNaN(n)) {
+      return 'not number'
+    }
+    switch (n) {
+      case 1:
+        return '1st'
+      case 2:
+        return '2nd'
+      case 3:
+        return '3rd'
+      default:
+        return `${n}th`
+    }
+  }, '')
+
+  return (
+    <Fragment>
+      <input
+        type="text"
+        onChange={(e) => {
+          setState(e.target.value)
+        }}
+      />
+      <p>{state}</p>
+    </Fragment>
+  )
+}
+
+export const App: VFC = () => (
+  <Fragment>
+    <h2>Counter Example with string prefix "count: "</h2>
+    <CounterExample />
+    <h2>English Counter Example</h2>
+    <EnglishCounterExample />
+  </Fragment>
+)
